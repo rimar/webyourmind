7e4169a22c6dfb6fc5bccdb74da9efcaf5f21734
rimar <rimar@hren.(none)>
2009-05-13 23:50:24 +0300

    move to h2 and order in pom

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,14 +1,25 @@
 import junit.framework.TestCase;
 
-import java.util.Date;
+import java.util.*;
 
 /**
  * @author me
  */
 public class Test extends TestCase {
 
-    public void testSomething() {
+    public void testSomething() throws Exception{
         System.out.println("==========================TEST=============================");
         System.out.println("Date:" + new Date());
+        Map<Thread,StackTraceElement[]> all = Thread.getAllStackTraces();
+        Set<Thread> trs = all.keySet();
+        for (Thread tr : trs) {
+          System.out.println("\nThread: " + tr.getName());
+          for (StackTraceElement element : all.get(tr)) {
+            System.out.println("  " + element);
+          }
+        }
+        /*synchronized(this) {
+          wait();
+        }*/
     }
 }
